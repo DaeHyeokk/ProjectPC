ce2bc93dd653329a64bbdeed9517132bc79667ba
JH.H <74130565+JIHEON-HYUN@users.noreply.github.com>
2025-08-12 12:03:50 +0900

    Folder Init

---
+++ Source/ProjectPC/Public/UI/Shop/PCShopWidget.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "PCShopWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTPC_API UPCShopWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
