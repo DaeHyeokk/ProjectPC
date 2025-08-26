e07c50c72dc1e6b86b86456b70013020c532245f
JH.H <74130565+JIHEON-HYUN@users.noreply.github.com>
2025-08-26 10:22:20 +0900

    no message

---
+++ Source/ProjectPC/Private/GameFramework/HelpActor/PCCarouselRing.cpp
@@ -0,0 +1,150 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameFramework/HelpActor/PCCarouselRing.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/RotatingMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/KismetMathLibrary.h"
+
+
+// Sets default values
+APCCarouselRing::APCCarouselRing()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+	RotatingMovement = CreateDefaultSubobject<URotatingMovementComponent>(TEXT("RotatingMovement"));
+	RotatingMovement->RotationRate = FRotator(0.f, RotationRateYawDeg, 0.f);
+	RotatingMovement->bRotationInLocalSpace = false;
+
+	// 카메라
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->TargetArmLength = CameraArmLength;
+	SpringArm->SetRelativeLocation(CameraArmLocalLocation);
+	SpringArm->SetRelativeRotation(CameraArmLocalRotation);
+
+	SpringArm->bInheritPitch = bCameraInheritActorRotation;
+	SpringArm->bInheritYaw = bCameraInheritActorRotation;
+	SpringArm->bInheritRoll = bCameraInheritActorRotation;
+
+	CarouselCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("CarouselCamera"));
+	CarouselCamera->SetupAttachment(SpringArm);
+	CarouselCamera->FieldOfView = CameraFov;
+
+	bReplicates = true;
+	SetReplicateMovement(true);
+
+}
+
+void APCCarouselRing::BeginPlay()
+{
+	Super::BeginPlay();
+	RotatingMovement->RotationRate.Yaw = RotationRateYawDeg;
+	RotatingMovement->SetActive(bRotate);
+
+	if (HasAuthority() && PickupClass && NumPickups > 0)
+	{
+		SpawnPickUps();
+		SetRotationActive(bRotate);
+	}
+
+	// 카메라 파라미터 반영(에디터에서 바꾼 값 유지)
+	SpringArm->TargetArmLength = CameraArmLength;
+	SpringArm->SetRelativeLocation(CameraArmLocalLocation);
+	SpringArm->SetRelativeRotation(CameraArmLocalRotation);
+	SpringArm->bInheritPitch = bCameraInheritActorRotation;
+	SpringArm->bInheritYaw   = bCameraInheritActorRotation;
+	SpringArm->bInheritRoll  = bCameraInheritActorRotation;
+	CarouselCamera->FieldOfView = CameraFov;
+}
+
+
+void APCCarouselRing::SetRotationActive(bool bOn)
+{
+	bRotate = bOn;
+	if (RotatingMovement)
+		RotatingMovement->SetActive(bRotate);
+}
+
+void APCCarouselRing::SpawnPickUps()
+{
+	if (!HasAuthority() || !PickupClass)
+		return;
+	ClearPickUps();
+
+	const int32 Count = FMath::Clamp(NumPickups, 1, NumSlots > 0 ? NumSlots : NumPickups);
+	for (int32 i = 0; i < Count; ++i)
+	{
+		FTransform PlayerTransform = GetSlotTransformWorld(i);
+		PlayerTransform.AddToTranslation(PickupOffset);
+				
+	}
+}
+
+void APCCarouselRing::ClearPickUps()
+{
+	if (!HasAuthority())
+		return;
+	for (auto& Weak : SpawnedPickupActors)
+	{
+		if (AActor* Actor = Weak.Get())
+		{
+			Actor->Destroy();
+		}
+	}
+	SpawnedPickupActors.Reset();
+}
+
+
+FTransform APCCarouselRing::GetSlotTransformWorld(int32 Index) const
+{
+	const int32 SlotNum = FMath::Max(1, NumSlots);
+	const float Step = 360.f / SlotNum;
+	const float Deg = StartAngleDeg + Step * (Index % SlotNum);
+	const float Rad = FMath::DegreesToRadians(Deg);
+
+	const FVector Local(FMath::Cos(Rad)*Radius, FMath::Sin(Rad)* Radius, Height);
+	const FVector WorldPosition = GetActorTransform().TransformPosition(Local);
+
+	FRotator Rotation = GetActorRotation();
+	if (bFaceCenter)
+	{
+		Rotation = (GetActorLocation() - WorldPosition).Rotation();
+	}
+	return FTransform(Rotation, WorldPosition, FVector::OneVector);
+}
+
+void APCCarouselRing::ApplyCentralView(APlayerController* PlayerController, float BlendTime)
+{
+	if (!PlayerController)
+		return;
+	PlayerController->SetViewTargetWithBlend(this, BlendTime);
+}
+
+
+#if WITH_EDITOR
+void APCCarouselRing::OnConstruction(const FTransform& transform)
+{
+	if (!bDrawDebug)
+		return;
+
+	UWorld* World = GetWorld();
+	if (!World)
+		return;
+
+	DrawDebugCircle(World, GetActorLocation(), Radius, 64, DebugColor, false, 0.f,0,2.f,
+		GetActorRightVector(), GetActorForwardVector());
+
+	for (int32 i = 0; i < NumSlots; ++i)
+	{
+		const FTransform T = GetSlotTransformWorld(i);
+		DrawDebugSphere(World, T.GetLocation(), 14.f, 12, DebugColor, false, 0.f,0,2.f);
+	}
+}
+
+#endif
---
+++ Source/ProjectPC/Public/GameFramework/HelpActor/PCCarouselRing.h
@@ -0,0 +1,122 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PCCarouselRing.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+class URotatingMovementComponent;
+
+UCLASS()
+class PROJECTPC_API APCCarouselRing : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	APCCarouselRing();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ring")
+	FName RingName;
+
+	// 슬롯 / 지오메트리
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ring")
+	int32 NumSlots = 8;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ring")
+	float Radius = 900.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ring")
+	float Height = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ring")
+	float StartAngleDeg = 90.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ring")
+	bool bFaceCenter = true;
+
+	// 회전
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotate")
+	bool bRotate = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotate")
+	float RotationRateYawDeg = 35.f;
+
+	// 회전초밥에 올릴 기물 설정
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PickUp")
+	TSubclassOf<AActor> PickupClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PickUp")
+	int32 NumPickups = 10;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PickUp")
+	FVector PickupOffset = FVector(0,0,30);
+
+	// 스폰, 정리, 회전제어
+
+	UFUNCTION(BlueprintCallable, Category = "Carousel")
+	void SpawnPickUps();
+
+	UFUNCTION(BlueprintCallable, Category = "Carousel")
+	void ClearPickUps();
+
+	UFUNCTION(BlueprintCallable, Category = "Carousel")
+	void SetRotationActive(bool bOn);
+
+	// 중앙 카메라
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	bool bCameraInheritActorRotation = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta=(ClampMin = "100.0"))
+	float CameraArmLength = 2000.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	FVector CameraArmLocalLocation = FVector(0,0,1400);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera")
+	FRotator CameraArmLocalRotation = FRotator(-55,0,0);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Camera", meta=(ClampMin="5.0", ClampMax="120.0"))
+	float CameraFov = 55.f;
+
+	// 슬롯 변환(월드)
+	UFUNCTION(BlueprintCallable, Category = "Ring")
+	FTransform GetSlotTransformWorld(int32 Index) const;
+
+	// 중앙 카메라로 보기
+	UFUNCTION(BlueprintCallable, Category = "Camera")
+	void ApplyCentralView(APlayerController* PlayerController, float BlendTime = 0.4f);
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere)
+	URotatingMovementComponent* RotatingMovement = nullptr;
+
+	UPROPERTY(Transient)
+	TArray<TWeakObjectPtr<AActor>> SpawnedPickupActors;
+
+	// 카메라 구성요소
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	USpringArmComponent* SpringArm = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CarouselCamera = nullptr;
+
+#if WITH_EDITOR
+public:
+	virtual void OnConstruction(const FTransform& transform) override;
+	
+	bool bDrawDebug = true;
+	FColor DebugColor = FColor::Green;
+
+	
+#endif
+	
+	
+};
